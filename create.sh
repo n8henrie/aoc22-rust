56d49b9d3f0c7171ceb342af5be98e96cffb7b9d
Nathan Henrie <nate@n8henrie.com>
2022-12-10 16:34:14 -0700

    Add convenience script to set up next puzzle

---
+++ create.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -Eeuf -o pipefail
+shopt -s inherit_errexit
+set -x
+
+main() {
+  local day=$1
+  local zero_padded_day=$(printf '%02d' "${day}")
+  local dir=$(printf 'd%s' "${zero_padded_day}")
+  cp -a ./template/. "${dir}"
+
+  sed -i "/^${dir}$/d" ./.gitignore
+  sed -i "s/# \(\"${dir}\",\)/\1/" ./Cargo.toml
+  sed -i 's/\(println!("day \)\(part \)/\1'"${zero_padded_day}"' \2/' ./"${dir}"/src/main.rs
+  sed -i 's/name = "d/&'"${zero_padded_day}"'/' ./"${dir}"/Cargo.toml
+
+  pushd "${dir}"
+  aoc download --input-file input.txt -d "${day}"
+}
+
+main "$@"
