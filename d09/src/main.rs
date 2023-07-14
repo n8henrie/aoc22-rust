a768494d0b5be33c8613afa922af5d3c84ff17ed
Nathan Henrie <nate@n8henrie.com>
2023-07-14 07:20:42 -0600

    Start d09

---
+++ d09/src/main.rs
@@ -0,0 +1,37 @@
+// #![warn(clippy::pedantic)]
+use aoc::{err, localpath, parse_input, Error, Result};
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn part1() -> Result<u32> {
+    todo!()
+}
+
+fn part2() -> Result<u32> {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    // let input = parse_input!(localpath!("input.txt"))?;
+    // println!("day 09 part 1: {}", part1(&input)?);
+    // println!("day 09 part 2: {}", part2(&input)?);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static EXAMPLE_INPUT: &str = r#"
+"#;
+
+    #[test]
+    fn test_part1() {
+        assert!(false);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert!(false);
+    }
+}
